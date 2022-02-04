ed53f959582f2097de97e61886d06f98df0c01b5
ouwargui <guikemon2@gmail.com>
2022-02-04 17:49:33 -0300

    chore: remove unused code

--- routes/Routes.tsx
+++ routes/Routes.tsx
@@ -5,29 +5,6 @@ import PersonalData from '../pages/PersonalData';
 
 const Stack = createStackNavigator();
 
-interface cardStyleInterpolatorProps {
-  gestureDirection: string;
-  cardStyleInterpolator: (current: any, layouts: any) => any;
-}
-
-const horizontalAnimation: cardStyleInterpolatorProps = {
-  gestureDirection: 'horizontal',
-  cardStyleInterpolator: ({current, layouts}) => {
-    return {
-      cardStyle: {
-        transform: [
-          {
-            translateX: current.progress.interpolate({
-              inputRange: [0, 1],
-              outputRange: [layouts.screen.width, 0],
-            }),
-          },
-        ],
-      },
-    };
-  },
-};
-
 const Routes: React.FC = () => {
   return (
     <Stack.Navigator screenOptions={{gestureDirection: 'horizontal', gestureEnabled: true}}>
